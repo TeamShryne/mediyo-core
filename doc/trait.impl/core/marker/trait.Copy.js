639f547838f396a93715ac27e9bb9ec9473902c6
TeamShryne <TeamShryne@users.noreply.github.com>
2026-08-21 03:53:21 +0000

    deploy: ca63f93e79088d34c759e47f55d9b16df7b8a23a

--- doc/trait.impl/core/marker/trait.Copy.js
+++ doc/trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["mediyo_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"mediyo_core/api/library/enum.LikeStatus.html\" title=\"enum mediyo_core::api::library::LikeStatus\">LikeStatus</a>",0]]]]);
+    const implementors = Object.fromEntries([["mediyo_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"mediyo_core/api/library/enum.LikeStatus.html\" title=\"enum mediyo_core::api::library::LikeStatus\">LikeStatus</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
